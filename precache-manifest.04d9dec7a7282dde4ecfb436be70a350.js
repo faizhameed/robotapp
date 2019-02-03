1841890b93515b2b60258c78c178d7d1306f02d3
Faiz Hameed <faizftb@gmail.com>
2019-02-03 22:32:53 +0300

    Updates

---
+++ precache-manifest.04d9dec7a7282dde4ecfb436be70a350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6848cdb4149dfea5f7a8",
+    "url": "/robotapp/static/js/runtime~main.6848cdb4.js"
+  },
+  {
+    "revision": "77668f47cd7fa54b1302",
+    "url": "/robotapp/static/js/main.77668f47.chunk.js"
+  },
+  {
+    "revision": "7dfbec9ba981eb594a59",
+    "url": "/robotapp/static/js/1.7dfbec9b.chunk.js"
+  },
+  {
+    "revision": "77668f47cd7fa54b1302",
+    "url": "/robotapp/static/css/main.e971a4d6.chunk.css"
+  },
+  {
+    "revision": "7dfbec9ba981eb594a59",
+    "url": "/robotapp/static/css/1.ea38065b.chunk.css"
+  },
+  {
+    "revision": "aec2f8266294ae96c23a6dfbad527697",
+    "url": "/robotapp/index.html"
+  }
+];
